1f1c06fc63b3f776e2713830e744b6459e31cdf4
jManock <jonmanock@gmail.com>
2018-04-10 17:10:33 -0400

    form setup

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ var main = require('./app/main');
 var header = require('./app/header');
 var title = require('./app/title');
 var footer = require('./app/footer');
+var form = require('./app/form');
 
 require('./index.css');
 
@@ -14,4 +15,5 @@ angular
   .component('app', main)
   .component('fountainHeader', header)
   .component('fountainTitle', title)
-  .component('fountainFooter', footer);
+  .component('fountainFooter', footer)
+  .component('fountainForm', form);

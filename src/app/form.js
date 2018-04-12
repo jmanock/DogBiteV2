0b11b385695f4dba841bdab96401becf2c92434e
jManock <jonmanock@gmail.com>
2018-04-12 13:59:34 -0400

    no errors think i might be lucky enough to have figured it out

--- src/app/form.js
+++ src/app/form.js
@@ -1,3 +1,12 @@
 module.exports = {
-  template: require('./form.html')
+  template: require('./form.html'),
+  controller: DogForm
 };
+
+function DogForm($scope, $log) {
+  $scope.$log = $log;
+  $log.log('Does this do anything?');
+  $scope.myFun = function () {
+    $log.log('Hi');
+  };
+}

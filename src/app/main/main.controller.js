a3d41e6e901b8fb5dd5740698847d880b3fe44bf
jManock <jonmanock@gmail.com>
2018-05-16 07:33:31 -0400

    this might work

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,92 +6,39 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($log) {
+  function MainController($log, $scope) {
     var vm = this;
-
-    // vm.something=true;
-    // vm.records = [
-    //   {name:'Jon Smith', city:'Manhattan', state:'Ny'},
-    //   {name:'Maria Rodriguez', city:'Brooklyn', state:'Ny'},
-    //   {name:'Michael Garcia', city:'Queens', state:'Ny'}
-    // ];
-    // vm.add = function($index) {
-    //   vm.records.push({name:'', city:'', state:''});
-    // }
-    // vm.remove = function(record, $index){
-    //   vm.records.splice($index,1);
-    //   $log.log(record);
-    // }
-    // vm.edit = function($event,$index){
-    //   //should only be able to edit one row
-    //   //vm.something=false;
-    //   $log.log($event, $index);
-    // }
-    vm.model = {
-      contacts:[
-        {id:1, name:'Ben', age:'28', state:'New York'},
-        {id:2, name:'Sally', age:'24', state:'Iowa'},
-        {id:3, name:'John', age:'32', state:'Georiga'},
-        {id:4, name:'Jane', age:'40', state:'New York'}
-      ],
-      selected:{}
-    };
-    vm.getTemplate = function(contact){
-      if(contact.id === vm.model.selected.id){
-        return 'edit';
-      }else{
-        return 'display';
-      }
-    };
-    vm.editContact = function(contact){
-      vm.model.selected = angular.copy(contact);
-    };
-    vm.saveContact = function(idx){
-      // $log.log('Saving contact');
-       vm.model.contacts[idx] = angular.copy(vm.model.selected);
-      //$log.log(vm.model.contacts[idx]);
-      vm.reset();
-    };
-    vm.reset = function(){
-      vm.model.selected = {};
-    };
-    vm.add = function(){
-      // Id should be plus one
-      var id = vm.model.contacts.id;
-      id = vm.model.contacts.length + 1;
-      $log.log(id);
-      vm.model.contacts.push({id:id, name:'', age:'', state:''});
-    };
-    vm.delete = function(name){
-      // Need to change the id number
-      // Need to remove row
-      var index = -1;
-      var comArr = eval(vm.model.contacts);
-      for (var i = 0; i<comArr.length; i++){
-        if(comArr[i].name === name){
-          index = i;
-          break;
-        }
-      }
-      if(index === -1){
-        alert('Something went wrong');
-      }
-      vm.model.contacts.splice(index, 1);
-    }
-
-    vm.dogs = [
-      {name:'', state:'', email:''}
+    $scope.title = 'ToDo';
+    $scope.tasks = [
+      {title:'take it out', complete:false},
+      {title:'Wash', complete:false}
     ];
-
-    vm.Next = function(dog){
-      vm.results = true;
-      vm.dogs.push(dog);
+    $scope.tasksComplete = [
+      {title:'Something alot', complete:true},
+      {title:'Dogs', complete:true}
+    ];
+    $scope.createTask = function(){
+      var task = [];
+      $scope.tasks.unshift({
+        title:$scope.newTitle,
+        date:Date.now()
+      });
+      $scope.newTitle = '';
     };
-    vm.Add = function(){
-      
+    $scope.removeTask = function(index){
+      var task = $scope.tasks[index];
+      $scope.tasks.splice($scope.tasks.indexOf(task),1);
     };
-    vm.Save = function(){
-
+    $scope.completeTask = function(index){
+      var task = $scope.tasks[index];
+      task.complete = true;
+      $scope.tasks.splice($scope.tasks.indexOf(task),1);
+      $scope.tasksComplete.unshift(task);
+    };
+    $scope.uncompleteTask = function(index){
+      var task = $scope.tasksComplete[index];
+      $scope.tasksComplete.splice($scope.tasksComplete.indexOf(task),1);
+      $scope.tasks.unshift(task);
     };
   }
 })();

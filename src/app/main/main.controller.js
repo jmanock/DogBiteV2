c1215a66e17e67feb773b659ada07fac816e3d94
jManock <jonmanock@gmail.com>
2018-05-16 15:54:48 -0400

    this might do the trick

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,43 +7,45 @@
 
   /** @ngInject */
   function MainController($log, $scope) {
-    var vm = this;
-    $scope.title = 'ToDo';
-    $scope.tasks = [
-      
-    ];
-    $scope.tasksComplete = [
+    var ctrl = this;
+    // $scope.title = 'ToDo';
+    // $scope.tasks = [
+    //
+    // ];
+    // $scope.tasksComplete = [
+    //
+    // ];
+    // $scope.createTask = function(){
+    //   var task = [];
+    //   $scope.tasks.unshift({
+    //     title:$scope.newTitle
+    //   });
+    //   $scope.newTitle = '';
+    // };
+    // $scope.removeTask = function(index){
+    //   var task = $scope.tasks[index];
+    //   $scope.tasks.splice($scope.tasks.indexOf(task),1);
+    // };
+    // $scope.Edit = function(index){
+    //   $log.log(index);
+    // };
+    // $scope.uncompleteTask = function(index){
+    //   var task = $scope.tasksComplete[index];
+    //   $scope.tasksComplete.splice($scope.tasksComplete.indexOf(task),1);
+    //   $scope.tasks.unshift(task);
+    // };
+    // $scope.taskLength = function(){
+    //   return $scope.tasks.length + $scope.tasksComplete.length;
+    // };
+    // $scope.taskCompletionTotal = function(unit){
+    //   var total = $scope.taskLength();
+    //   return Math.floor(100/total * unit);
+    // };
 
-    ];
-    $scope.createTask = function(){
-      var task = [];
-      $scope.tasks.unshift({
-        title:$scope.newTitle,
-        date:Date.now()
-      });
-      $scope.newTitle = '';
-    };
-    $scope.removeTask = function(index){
-      var task = $scope.tasks[index];
-      $scope.tasks.splice($scope.tasks.indexOf(task),1);
-    };
-    $scope.completeTask = function(index){
-      var task = $scope.tasks[index];
-      task.complete = true;
-      $scope.tasks.splice($scope.tasks.indexOf(task),1);
-      $scope.tasksComplete.unshift(task);
-    };
-    $scope.uncompleteTask = function(index){
-      var task = $scope.tasksComplete[index];
-      $scope.tasksComplete.splice($scope.tasksComplete.indexOf(task),1);
-      $scope.tasks.unshift(task);
-    };
-    $scope.taskLength = function(){
-      return $scope.tasks.length + $scope.tasksComplete.length;
-    };
-    $scope.taskCompletionTotal = function(unit){
-      var total = $scope.taskLength();
-      return Math.floor(100/total * unit);
-    };
+    ctrl.tasklist = [];
+    ctrl.addTask = function(){
+      ctrl.tasklist.push(ctrl.taskInput);
+      ctrl.taskInput = '';
+    }
   }
 })();

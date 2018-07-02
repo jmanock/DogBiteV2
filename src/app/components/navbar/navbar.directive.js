a75e65bf9d7af40304ea2b74822775dec55a7ebf
jManock <jonmanock@gmail.com>
2018-07-02 12:13:33 -0400

    setting up the routes

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -1,32 +1,25 @@
-(function() {
+(function(){
   'use strict';
 
   angular
     .module('dogBiteV2')
     .directive('acmeNavbar', acmeNavbar);
 
-  /** @ngInject */
-  function acmeNavbar() {
+  function acmeNavbar(){
     var directive = {
-      restrict: 'E',
-      templateUrl: 'app/components/navbar/navbar.html',
-      scope: {
-          creationDate: '='
+      restrict:'E',
+      templateUrl:'app/components/navbar/navbar.html',
+      scope:{
+        creationDate:'='
       },
-      controller: NavbarController,
-      controllerAs: 'vm',
-      bindToController: true
+      controller:NavbarController,
+      controllerAs:'vm',
+      bindToController:true
     };
-
     return directive;
 
-    /** @ngInject */
-    function NavbarController(moment) {
+    function NavbarController(moment){
       var vm = this;
-
-      // "vm.creationDate" is available by directive option "bindToController: true"
-      vm.relativeDate = moment(vm.creationDate).fromNow();
     }
   }
-
 })();
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,6 +13,49 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('about', {
+        url:'/about',
+        templateUrl:'app/main/about.html'
+      })
+      .state('privacy', {
+        url:'/privacy',
+        templateUrl:'app/main/privacy.html'
+      })
+      .state('questions', {
+        url:'/questions',
+        templateUrl:'app/main/questions.html'
+      })
+      .state('terms', {
+        url:'/terms',
+        templateUrl:'app/main/terms.html'
+      })
+      .state('form', {
+        url:'/form',
+        templateUrl:'app/form/form.html',
+        controller:'FormController',
+        controllerAs:'form'
+      })
+      .state('summary', {
+        url:'/summary',
+        templateUrl:'app/form/summary.html',
+        controller:'SummaryController',
+        controllerAs:'summary',
+        params:{'dogs':null}
+      })
+      .state('insurance', {
+        url:'/insurance',
+        templateUrl:'app/form/insurance.html',
+        controller:'InsuranceController',
+        controllerAs:'insurance',
+        params:{'dog':null, 'insurance':null}
+      })
+      .state('total', {
+        url:'/total',
+        templateUrl:'app/form/total.html',
+        controller:'TotalController',
+        controllerAs:'total',
+        params:{'insurance':null, 'dog':null}
       });
 
     $urlRouterProvider.otherwise('/');
